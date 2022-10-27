5b26bd9b4f361f1a66f8a3a271f00a6de6938694
Lynh <chinsugaming@gmail.com>
2022-10-27 14:55:02 +0700

    thuc hanh

--- App.js
+++ App.js
@@ -1,66 +1,45 @@
-// import { StatusBar } from "expo-status-bar";
-// import { StyleSheet, Text, View } from "react-native";
-// import AppNavigation from "./src/components/AppNavigation";
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
-// export default function App() {
-//   return <AppNavigation />;
-// }
+import Swiper from "react-native-swiper";
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
-import * as React from "react";
-import { View, useWindowDimensions } from "react-native";
-import { TabView, SceneMap } from "react-native-tab-view";
-
-const FirstRoute = () => (
-  <View style={{ flex: 1, backgroundColor: "#ff6600" }} />
-);
-
-const SecondRoute = () => (
-  <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
-);
-const ThirdRoute = () => (
-  <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
-);
-const FourthRoute = () => (
-  <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
-);
-const FifthRoute = () => (
-  <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
-);
-
-const renderScene = SceneMap({
-  first: FirstRoute,
-  second: SecondRoute,
-  third: ThirdRoute,
-  fourth: FourthRoute,
-  fifth: FifthRoute,
+const styles = StyleSheet.create({
+  wrapper: {},
+  slide: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FF6600",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
 });
 
-export default function TabViewExample() {
-  const layout = useWindowDimensions();
-
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: "first", title: "First" },
-    { key: "second", title: "Second" },
-    { key: "third", title: "Third" },
-    { key: "fourth", title: "Fourth" },
-    { key: "fifth", title: "fifth" },
-  ]);
-
-  return (
-    <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={{ width: layout.width }}
-    />
-  );
+export default class SwiperComponent extends Component {
+  render() {
+    return (
+      <Swiper style={styles.wrapper}>
+        <View style={styles.slide}>
+          <Text style={styles.text}>Board 1</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text style={styles.text}>Board 2</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text style={styles.text}>Board 3</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text style={styles.text}>Board 4</Text>
+        </View>
+        <View style={styles.slide}>
+          <Text style={styles.text}>Board 5</Text>
+        </View>
+      </Swiper>
+    );
+  }
 }
+
+AppRegistry.registerComponent("myproject", () => SwiperComponent);
